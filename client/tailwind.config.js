242324ef0d6705dcd7f012af74cc44a888b893a8
Adam <aearanha@hotmail.com>
2022-01-24 22:32:07 -0500

    Added config file for tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  mode: 'jit',
+  // purge:['./src/**/*.{js,jsx,ts,tsx}','./public/index/'],
+  theme: { extend: {}, },
+  plugins: [],
+}
