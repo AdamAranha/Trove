6d269dd54776347fc098c096e90e5865b6cdac52
Adam <aearanha@hotmail.com>
2022-02-08 10:18:10 -0500

    Testing popup for password validation on login screen

--- client/src/components/Landing/Login.tsx
+++ client/src/components/Landing/Login.tsx
@@ -15,9 +15,12 @@ const Login: React.FC<Props> = ({ showLogin, setShowLogin, setShowSignup }) => {
         email: '',
         password: ''
     });
-    const [emailErr, setEmailErr] = useState('');
-    const [passwordErr, setPasswordErr] = useState('');
+    const [valErr, setValErr] = useState({
+        emailErr: '',
+        passErr: ''
+    });
     const [showPassword, setShowPassword] = useState(false);
+    const [passVal, setPassVal] = useState(false);
 
 
     function changePage(): void {
@@ -25,8 +28,7 @@ const Login: React.FC<Props> = ({ showLogin, setShowLogin, setShowSignup }) => {
         setShowSignup(true);
         setCredentials({ email: '', password: '' });
         setShowPassword(false);
-        setEmailErr('');
-        setPasswordErr('');
+        setValErr({ emailErr: '', passErr: '' })
     }
 
     function handleChange(event: any): void {
@@ -41,15 +43,32 @@ const Login: React.FC<Props> = ({ showLogin, setShowLogin, setShowSignup }) => {
 
     function handleSubmit(event: any): void {
         event.preventDefault();
-        setEmailErr('');
-        setPasswordErr('');
+        setValErr({ emailErr: '', passErr: '' })
         const { email, password } = credentials;
         const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         const regexPassword = /^(.{0,7}|[^A-Z]{1,}|[^a-z]{1,}|[^\d]{1,})$|[\s]/
-        if (!regexEmail.test(email)) setEmailErr('*Please enter a valid email address.');
-        if (regexPassword.test(password)) setPasswordErr('*Password must be atleast 8 characters, and contain atleast 1 uppercase letter, 1 lowercase letter, and 1 number.');
+        if (!regexEmail.test(email)) {
+            setValErr(prevInput => {
+                return {
+                    ...prevInput,
+                    emailErr: '*Please enter a valid email.'
+                }
+            });
+        }
+        if (regexPassword.test(password)) {
+            setValErr(prevInput => {
+                return {
+                    ...prevInput,
+                    passErr: '*Please enter a valid password.'
+                }
+            });
+        }
         // Checks for special characters
         // const regex2 = /[^((0-9)|(a-z)|(A-Z)|\s)]/
+    }
+
+    function passwordValidation() {
+        console.log('On focus works!')
     }
 
     // return (
@@ -83,22 +102,33 @@ const Login: React.FC<Props> = ({ showLogin, setShowLogin, setShowSignup }) => {
         showLogin ?
             <div className='flex flex-col text-yellow-50 p-7 rounded justify-center 
            2xl:bg-slate-800
-            2xl:h-[800px] 2xl:w-[700px] 2xl:p-20'>
+            2xl:h-[800px] 2xl:w-[700px] 2xl:p-20 2xl:justify-self-center 2xl:self-center'
+            >
+                {/* <div className='h-screen'></div> */}
                 <h2 className='text-4xl font-medium mb-2'>Log in.</h2>
-                <p className='text-sm mb-2 2xl:block'>Enter your email address and a password to create an account.</p>
+                <p className='mb-7 2xl:block'>Enter your email address and a password to create an account.</p>
                 <form className='flex flex-col' onSubmit={(event) => handleSubmit(event)}>
                     <label className='mb-1'>Email</label>
                     <input className='p-2 rounded text-slate-800 w-full' name='email' onChange={event => handleChange(event)} value={credentials.email} />
-                    <p className='mb-7 text-red-400'> {emailErr} </p>
+                    <p className='h-1 mb-7 text-red-400'>{valErr.emailErr}</p>
                     <label className='mb-1'>Password</label>
-                    <div className='relative w-fit'>
-                        <input className='p-2 rounded text-slate-800 w-64' name='password' type={showPassword ? 'text' : 'password'} onChange={(event) => handleChange(event)} value={credentials.password} />
+                    <div className='relative w-full'>
+                        {passVal ?
+                            <div className='absolute w-full h-48 -top-56 bg-slate-700 rounded p-6 leading-7'>
+                                <p>Atleast 8 characters in length</p>
+                                <p>Must Contain:</p>
+                                <p className='indent-4'>An Upper case letter (A-Z)</p>
+                                <p className='indent-4'>A Lower case letter (1-z)</p>
+                                <p className='indent-4'>A number (0-9)</p>
+                            </div>
+                            : null}
+                        <input className='p-2 rounded text-slate-800 w-full' name='password' type={showPassword ? 'text' : 'password'} onChange={(event) => handleChange(event)} onFocus={() => setPassVal(true)} onBlur={() => setPassVal(false)} value={credentials.password} />
                         <img className='absolute h-8 w-8 top-1 right-2' src={showPassword ? eye : eyeOff} alt={showPassword ? 'Password is visible' : 'Password is not visible'} onClick={() => setShowPassword(!showPassword)} />
                     </div>
-                    <p className='mb-7 text-red-400'> {passwordErr} </p>
+                    <p className='h-1 mb-7 text-red-400'>{valErr.passErr}</p>
                 </form>
-                <p className='text-sm text-center'>Don't have an account?<span className='cursor-pointer font-semibold text-emerald-400 hover:text-emerald-500' onClick={changePage}> Create an account</span></p>
-                <button className='sticky b-0 w-full bg-emerald-500 self-center hover:bg-emerald-600 text-slate-800  font-semibold rounded p-2'>Log in</button>
+                <button className='sticky b-0 w-full mb-2 bg-emerald-500 self-center hover:bg-emerald-600 text-slate-800  font-semibold rounded p-2' onClick={handleSubmit}>Log in</button>
+                <p className='text-center'>Don't have an account?<span className='cursor-pointer font-semibold text-emerald-400 hover:text-emerald-500' onClick={changePage}> Create an account</span></p>
             </div>
             : null
     )
