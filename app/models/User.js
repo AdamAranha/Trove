717dab4e24886b57f0304f5fdd1ab39e87fd315d
Adam <aearanha@hotmail.com>
2022-02-09 12:18:33 -0500

    Created model and db config info for sequelize

---
+++ app/models/User.js
@@ -0,0 +1,21 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../util/db');
+
+const User = sequelize.define('User', {
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }
+})
+
+module.exports = User;
