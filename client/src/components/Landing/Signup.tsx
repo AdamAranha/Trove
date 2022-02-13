8460b2ec316983acd619807ab3ee3ec0f8b2daa2
Adam <aearanha@hotmail.com>
2022-02-12 21:57:23 -0500

    Added fetch request for creating new user

--- client/src/components/Landing/Signup.tsx
+++ client/src/components/Landing/Signup.tsx
@@ -57,23 +57,26 @@ const Signup: React.FC<Props> = ({ showSignup, setShowSignup, setShowLogin }) =>
                 [name]: value
             }
         });
-        if (regexLength.test(value) || value === '') { validations('LENGTH', false) } else { validations('LENGTH', true) };
-        if (regexUpper.test(value) || value === '') { validations('UPPERCASE', false) } else { validations('UPPERCASE', true) }
-        if (regexLower.test(value) || value === '') { validations('LOWERCASE', false) } else { validations('LOWERCASE', true) }
-        if (regexNumber.test(value) || value === '') { validations('NUMBER', false) } else { validations('NUMBER', true) }
-        if ([...value].includes(' ') || value === '') { validations('WHITESPACE', false) } else { validations('WHITESPACE', true) }
+
+        function validations(name: string, value: boolean) {
+            setPassword(prevInput => {
+                return {
+                    ...prevInput,
+                    [name]: value
+                }
+            })
+        }
+
+        if (name === 'password') {
+            if (regexLength.test(value) || value === '') { validations('LENGTH', false) } else { validations('LENGTH', true) };
+            if (regexUpper.test(value) || value === '') { validations('UPPERCASE', false) } else { validations('UPPERCASE', true) }
+            if (regexLower.test(value) || value === '') { validations('LOWERCASE', false) } else { validations('LOWERCASE', true) }
+            if (regexNumber.test(value) || value === '') { validations('NUMBER', false) } else { validations('NUMBER', true) }
+            if ([...value].includes(' ') || value === '') { validations('WHITESPACE', false) } else { validations('WHITESPACE', true) }
+        }
     }
 
-    function validations(name: string, value: boolean) {
-        setPassword(prevInput => {
-            return {
-                ...prevInput,
-                [name]: value
-            }
-        })
-    }
-
-    function handleSubmit(event: any): void {
+    async function handleSubmit(event: any): Promise<any> {
         event.preventDefault();
         setValErr({ emailErr: '', passErr: '' })
         const { email, password } = credentials;
@@ -87,6 +90,25 @@ const Signup: React.FC<Props> = ({ showSignup, setShowSignup, setShowLogin }) =>
                 }
             });
         }
+        async function checkIfUserExists() {
+            const response = await fetch('http://localhost:3001/db/findUser', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    email
+                }
+            }).then(r => r.json());
+            return response[0] ? true : false
+        }
+        if (await checkIfUserExists()) {
+            setValErr(prevInput => {
+                return {
+                    ...prevInput,
+                    emailErr: '*This email is already in use.'
+                }
+            });
+        }
+
         if (regexPassword.test(password)) {
             setValErr(prevInput => {
                 return {
@@ -94,18 +116,35 @@ const Signup: React.FC<Props> = ({ showSignup, setShowSignup, setShowLogin }) =>
                     passErr: '*Please enter a valid password.'
                 }
             });
-        } else console.log('wtf')
+        }
+
+        async function createUser() {
+            const response = await fetch('http://localhost:3001/db/createUser', {
+                method: 'POST',
+                body: new URLSearchParams({
+                    'user': email,
+                    'password': password
+                })
+            }).then(r => r.json());
+            return response;
+        }
         // Checks for special characters
         // const regex2 = /[^((0-9)|(a-z)|(A-Z)|\s)]/
+        if (!await checkIfUserExists() && !regexPassword.test(password) && regexEmail.test(email)) {
+            console.log('This is sending');
+            const newUser = await createUser();
+            console.log(newUser);
+        }
+
     }
 
     return (
         showSignup ?
             <div className='flex flex-col text-yellow-50 p-7 rounded justify-center 
-           2xl:bg-slate-800
-            2xl:h-[800px] 2xl:w-[700px] 2xl:p-20 2xl:justify-self-center 2xl:self-center'>
-                <h2 className='text-4xl 2xl:text-5xl font-medium mb-2'>Sign up.</h2>
-                <p className='mb-7 2xl:block'>Enter the email address and password used to create an account.</p>
+           sm:bg-slate-800
+            sm:h-[800px] sm:w-[700px] sm:p-20 sm:justify-self-center sm:self-center'>
+                <h2 className='text-4xl sm:text-5xl font-medium mb-2'>Sign up.</h2>
+                <p className='mb-7 sm:block'>Enter the email address and password used to create an account.</p>
                 <form className='flex flex-col' onSubmit={(event) => handleSubmit(event)}>
                     <label className='mb-1'>Email</label>
                     <input className='p-2 rounded text-slate-800 w-full' name='email' onChange={event => handleChange(event)} value={credentials.email} />
@@ -113,13 +152,13 @@ const Signup: React.FC<Props> = ({ showSignup, setShowSignup, setShowLogin }) =>
                     <label className='mb-1'>Password</label>
                     <div className='relative w-full'>
                         {showPassVal ?
-                            <div className='grid absolute w-full h-56 -top-64 bg-slate-800 2xl:bg-slate-900 rounded shadow-black shadow-lg p-5 leading-7'>
+                            <div className='grid absolute w-full h-56 -top-64 bg-slate-800 sm:bg-slate-900 rounded shadow-black shadow-lg p-5 leading-7'>
                                 <p className='font-semibold'>Must Contain:</p>
-                                <p className={`indent-4 ${password.LENGTH ? 'text-emerald-400' : 'text-red-500 2xl:text-red-400'}`}>At least 8 characters in length</p>
-                                <p className={`indent-4 ${password.WHITESPACE ? 'text-emerald-400' : 'text-red-500 2xl:text-red-400'}`}>No whitespaces</p>
-                                <p className={`indent-4 ${password.UPPERCASE ? 'text-emerald-400' : 'text-red-500 2xl:text-red-400'}`}>An Upper case letter (A-Z)</p>
-                                <p className={`indent-4 ${password.LOWERCASE ? 'text-emerald-400' : 'text-red-500 2xl:text-red-400'}`}>A Lower case letter (a-z)</p>
-                                <p className={`indent-4 ${password.NUMBER ? 'text-emerald-400' : 'text-red-500 2xl:text-red-400'}`}>A number (0-9)</p>
+                                <p className={`indent-4 ${password.LENGTH ? 'text-emerald-400' : 'text-red-500 sm:text-red-400'}`}>At least 8 characters in length</p>
+                                <p className={`indent-4 ${password.WHITESPACE ? 'text-emerald-400' : 'text-red-500 sm:text-red-400'}`}>No whitespaces</p>
+                                <p className={`indent-4 ${password.UPPERCASE ? 'text-emerald-400' : 'text-red-500 sm:text-red-400'}`}>An Upper case letter (A-Z)</p>
+                                <p className={`indent-4 ${password.LOWERCASE ? 'text-emerald-400' : 'text-red-500 sm:text-red-400'}`}>A Lower case letter (a-z)</p>
+                                <p className={`indent-4 ${password.NUMBER ? 'text-emerald-400' : 'text-red-500 sm:text-red-400'}`}>A number (0-9)</p>
                             </div>
                             : null}
                         <input className='p-2 rounded text-slate-800 w-full' name='password' type={showPassword ? 'text' : 'password'} onChange={(event) => handleChange(event)} onFocus={() => setShowPassVal(true)} onBlur={() => setShowPassVal(false)} value={credentials.password} />
